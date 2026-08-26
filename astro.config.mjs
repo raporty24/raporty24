f139df1113c9562f1587b10856892fd2f2a2af08
Monika <monis.biala@gmail.com>
2026-08-26 14:32:20 +0200

    config poprawa

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,13 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   // KLUCZOWE DLA SEO: Adres Twojej domeny
-  // Niezbędny do generowania poprawnych linków w rss.xml oraz sitemap-index.xml
   site: 'https://raporty24.pl',
-  
+
   // Integracje Astro
   integrations: [
-    sitemap(), // Automatycznie generuje mapę strony przy każdym "build"
+    sitemap({
+      filter: (page) => page !== 'https://raporty24.pl/thanks/'
+    }),
   ],
 
   vite: {
